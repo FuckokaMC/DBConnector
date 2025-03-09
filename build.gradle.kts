79a08da80f62d419d3bf5dee55bd92605f0bfe24
deceitya <49361292+deceitya@users.noreply.github.com>
2025-03-09 21:58:15 +0900

    書き方整理&Kotlinバージョン更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
-    `maven-publish`
+    kotlin("jvm") version "2.1.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    `maven-publish`
 }
 
 group = "mc.fuckoka"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
-    maven {
-        name = "papermc"
-        url = uri("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
     }
 }
 
@@ -20,15 +22,15 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("com.zaxxer:HikariCP:6.2.1")
     compileOnly("com.mysql:mysql-connector-j:9.2.0")
-    testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
+val targetJavaVersion = 21
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
 }
 
 publishing {
